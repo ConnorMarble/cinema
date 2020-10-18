0adc28b450108a3928b25d115cba78801b4abe9b
Connor Marble <connor.marble@gmail.com>
2020-10-18 19:46:27 -0400

    updated

--- src/services/movies.service.js
+++ src/services/movies.service.js
@@ -1,5 +1,4 @@
 import axios from 'axios';
-
 const REQUEST_URL = 'https://api.themoviedb.org/3';
 export const IMAGE_URL = 'https://image.tmdb.org/t/p/original';
 const API_KEY = process.env.REACT_APP_API_SECRET;
